542cc6039b2cdc8be5e56590a5f7340e67bf334f
Philo Li <maerd.lll.p@gmail.com>
2023-03-20 13:47:30 +0000

    fix 026.py

--- 026.py
+++ 026.py
@@ -4,15 +4,20 @@ class Solution(object):
         :type nums: List[int]
         :rtype: int
         """
-        k = 0
-        for i in range(len(nums)):
-            if nums[i] != nums[i-1]:
-                nums[k] = nums[i]
-                k += 1
-            print(k, i, nums[k], nums[i])
-        return k
+        if len(nums) == 0:
+            return 0
+        last, finder = 0, 0
+        for last in range(len(nums) - 1):
+            while nums[last] == nums[finder]:
+                finder += 1
+                if finder == len(nums):
+                    return last + 1
+            # print(last, finder, nums[last], nums[finder])
+            nums[last+1] = nums[finder]
+            last += 1
+        return last + 1
 
 if __name__ == '__main__':
     solution = Solution()
-    print(solution.removeDuplicates([1,1,2]))
-    # print(solution.removeDuplicates([0,0,1,1,1,2,2,3,3,4]))
+    # print(solution.removeDuplicates([1,1,2]))
+    print(solution.removeDuplicates([0,0,1,1,1,2,2,3,3,4]))
