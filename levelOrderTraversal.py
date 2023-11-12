2baa21bc4161ddb0a01096418c459aa575694574
Philo Li <maerd.lll.p@gmail.com>
2023-11-13 05:24:59 +0900

    add level order traversal.py

---
+++ levelOrderTraversal.py
@@ -0,0 +1,46 @@
+# Binary Tree Lever Order Traversal
+# Definition for a binary tree node.
+class TreeNode(object):
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+class Solution(object):
+    def levelOrder(self, root):
+        """
+        :type root: TreeNode
+        :rtype: List[List[int]]
+        """
+        if not root:
+            return []
+        queue = coolections.deque([root])
+        result = []
+        while queue:
+            level = []
+            for _ in range(len(queue)):
+                cur = queue.popleft()
+                level.append(cur.val)
+                if cur.left:
+                    queue.append(cur.left)
+                if cur.right:
+                    queue.append(cur.right)
+            result.append(cur.level)
+        return result
+
+# 递归法
+class Solution:
+    def levelOrder2(self, root):
+        levels = []
+        self.helper(root, 0, levels)
+        return levels
+
+    def helper(self, node, level, levels):
+        if not node:
+            return
+        if len(levels) == level:
+            levels.append([])
+        levels[level].append(node.val)
+        self.helper(node.left, level + 1, levels)
+        self.helper(node.right, level + 1, levels)
+    
