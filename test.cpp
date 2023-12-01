fc17194b8335f056910ce5064dbf20f5aed3b400
Philo Li <maerd.lll.p@gmail.com>
2023-12-02 00:48:15 +0900

    cpp env setup

---
+++ test.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main ()
+{
+    int number{0};
+    number = 10 + 20;
+    cout << "Hello World! ";
+    cout << "I'm a C++ program" << endl;
+    cout << "number is : " << number << endl;
+    return 0;
+}
