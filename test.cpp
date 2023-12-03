6c39443f79152720750b2058681c95f5d6c6d5db
Philo Li <maerd.lll.p@gmail.com>
2023-12-03 17:41:42 +0900

    update test.cpp

--- test.cpp
+++ test.cpp
@@ -1,12 +1,32 @@
 #include <iostream>
+#include <vector>
+
 using namespace std;
 
-int main ()
-{
-    int number{0};
-    number = 10 + 20;
-    cout << "Hello World! ";
-    cout << "I'm a C++ program" << endl;
-    cout << "number is : " << number << endl;
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        int left = 0;
+        int right = nums.size() - 1;
+        while ( left <= right) {
+            int middle = left + ((right - left) / 2);
+            if (nums[middle] > target) {
+                right = middle - 1;
+            } else if (nums[middle] < target) {
+                left = middle + 1;
+            } else {
+                return middle;
+            }
+        }
+        return -1;
+    }
+};
+
+int main () {
+    Solution s;
+    vector<int> v{-1, 0, 3, 5, 9, 12};
+    int ans = s.search(v, 9);
+    cout << ans << endl;
+
     return 0;
 }
